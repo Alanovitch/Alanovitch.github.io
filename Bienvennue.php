2c9b7c326c77194fddd07c04633e2f08447303b8
Alanovitch <90872111+Alanovitch@users.noreply.github.com>
2021-09-17 01:57:48 +0200

    Add files via upload

---
+++ Bienvennue.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>!!WIP!! - Directory Search</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <div class="container" id="glass">
+        <div class="align-items-center justify-content-center row" style="min-height: 100vh;">
+            <div class="col-sm-6 text-center">
+                <form action="" method="get">
+                                    <h4 class="display-4">Directory Search</h4>
+                    <div class="form-group">
+                        <input type="text" name="username" class="form-control" id="username"
+                            placeholder="Search user..." required> 
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
